3724d0b70caf605ce2ed1a5c749e4c9143a6edb7
Joost Faber <j.faber@hva.nl>
2022-04-22 15:47:53 +0200

    relocated favicon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <svelte:head>
-    <link rel="icon" type="image/png" href="https://styleguide.fdnd.nl/favicon.png">
     <link rel="stylesheet" href="https://styleguide.fdnd.nl/fdnd.css">
 </svelte:head>
 
