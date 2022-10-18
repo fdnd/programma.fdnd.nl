f013908e0df5067ec4680fa0b090d116339b1d72
koopReynders <mail@kopo.nl>
2022-10-18 18:15:57 +0200

    <ul> verandert naar <menu> met styling

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,8 +1,24 @@
 <nav>
   <h2>Menu</h2>
-  <ul>
+  <menu>
     <li>
       <a class="active" data-sveltekit-prefetch href="/">Home</a>
     </li>
-  </ul>
+  </menu>
 </nav>
+
+<style>
+  nav{
+    max-width: 42rem;
+    display: flex;
+    justify-content:end;
+  }
+  menu {
+    list-style: none;
+  }
+  menu a {
+    display: block;
+    border-radius: 0.5rem;
+    transform: rotate(-5deg) translateY(-3rem);
+  }
+</style>
