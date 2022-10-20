c5046320361d836091c99f7bf6833ce06c35d404
koopReynders <mail@kopo.nl>
2022-10-20 12:16:33 +0200

    current sprint added

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,28 +4,42 @@
     <li>
       <a class="active" data-sveltekit-prefetch href="/">Home</a>
     </li>
+    <li>
+      <a href="/all-human">Sprint 3</a>
+    </li>
+    <li>
+      <a href="/choices-choices">Sprint 15</a>
+    </li>
   </menu>
 </nav>
 
 <style>
   nav{
+    /* background: gold; */
     max-width: 42rem;
     display: flex;
     justify-content:end;
+    font-size: 0.8rem;
+    margin-bottom: 2rem;
   }
   menu {
     list-style: none;
   }
-  menu a {
-    display: block;
-    border-radius: 0.5rem;
-    transform: rotate(-5deg) translateY(-6.8rem);
+  menu li{
+    display: inline-block;
   }
-
-  @media (min-width: 30em) {
+  menu a {
+    border-radius: 0.5rem;
+    transform: rotate(-5deg) ;
+  }
+  @media (min-width: 22em) {
+    nav{
+      font-size: 1rem;
+    }
+  }
+  @media (min-width: 42em) {
     menu a {
       transform: rotate(-5deg) translateY(-3rem);
     }
-
   }
 </style>
