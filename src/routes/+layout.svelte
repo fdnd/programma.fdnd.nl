6695dadddabb1142fa63c595b3bec12d05d69da6
ju5tu5 <justus@ju5tu5.nl>
2022-10-29 06:13:54 +0200

    Addded gtm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,28 @@
 <script>
-	import Nav from '$lib/components/Nav.svelte'
-	import Footer from '$lib/components/Footer.svelte'
+  import Nav from '$lib/components/Nav.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+  import GAnalytics from '$lib/components/GAnalytics.svelte';
+
+  export const gtmId = 'G-QWP67QPK22';
 </script>
 
 <svelte:head>
-	<title>FDND Programma</title>
+  <title>FDND Programma</title>
 </svelte:head>
 
+<GAnalytics {gtmId} />
+
 <header>
-	<h1><span>FDND</span> Programma</h1>
+  <h1><span>FDND</span> Programma</h1>
 </header>
 
 <Nav />
 
 <main>
-	<slot />
+  <slot />
 </main>
 
 <Footer />
 
 <style>
-
 </style>
