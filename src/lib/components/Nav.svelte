79cd198c8a4277c64343a13aa76d65ad819e3ea6
joostf <j.faber@hva.nl>
2022-11-15 16:03:14 +0100

    Added current sprints in nav
    Make dynamic later

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -10,12 +10,33 @@
             <a class="active" data-sveltekit-prefetch href="/">Home</a>
         </li>
         {/if}
+        <li>
+            <a data-sveltekit-reload href="/static-web/look-and-feel">Sprint 4</a>
+        </li>
+        <li>
+            <a data-sveltekit-reload href="/workflow-tooling-frameworks/dont-repeat-yourself">Sprint 16</a>
+        </li>
     </ul>
 </nav>
 
 <style>
-    nav {
-        margin-left: .5rem;
-        background:none
+nav {
+    max-width: 42rem;
+    display: flex;
+    font-size: 0.8rem;
+    background-color: transparent;
+}
+@media (min-width: 22em) {
+    nav{
+        font-size: 1rem;
     }
+}
+@media (min-width: 42em) {
+    nav{
+        margin-bottom: 0;
+    }
+    nav ul a {
+        transform: rotate(-5deg);
+    }
+}
 </style>
