8501333c90a9c588206b22f0831b66596920e68b
joostf <j.faber@hva.nl>
2022-11-17 11:55:06 +0100

    Worked on #13

--- src/lib/components/program/sprint/Tasks.svelte
+++ src/lib/components/program/sprint/Tasks.svelte
@@ -1,48 +1,73 @@
 <script>
-    import { onMount } from 'svelte'
-    import Heading from '$lib/components/Heading.svelte'
+import { onMount } from 'svelte'
+import Heading from '$lib/components/Heading.svelte'
+import IconStar from '$lib/components/icons/Star.svelte'
 
-    export let tasks
-    
-    onMount(() => { 
-        /* Firefox hack, :has() selector not supported */
-        if(!CSS.supports('selector(html:has(body))')){ 
-            const subtasks = document.querySelector('.tasks li')
-            const subtasksHeading = document.querySelector('.tasks h3')
-            if( subtasks) {
-                subtasksHeading.style.display = 'block'
-            }
-        } 
-	})   
+export let tasks
+
+onMount(() => { 
+    /* Firefox hack, :has() selector not supported */
+    if(!CSS.supports('selector(html:has(body))')){ 
+        const subtasks = document.querySelector('.tasks li')
+        const subtasksHeading = document.querySelector('.tasks h3.subtasks')
+        if( subtasks) {
+            subtasksHeading.style.display = 'block'
+        }
+    } 
+})   
 </script>
 
 {#if tasks.length > 0}
     <section class="green-on-blue col-span-1 tasks">
-      <Heading title="Leertaken" />
+        <Heading title="Leertaken" />
 
-      {#each tasks as task}
-        {#if task.topic == 'task'}
-          <p>
-            <a href="{task.url}" data-sveltekit-prefetch>{task.name}</a><small class="blue-on-green">{task.forkCount} forks</small>
-          </p>
-          <p>
-            {task.description}
-          </p>
-        {/if}
-      {/each}
-      
-      <h3>Deeltaken</h3>
-      <ul>
         {#each tasks as task}
-          {#if task.topic == 'subtask'}
-          <li>
-            
-            <a href="{task.url}" data-sveltekit-prefetch>{task.name}</a><small class="blue-on-green">{task.forkCount} forks</small>
-          </li>
-          {/if}
+            {#if task.topic == 'task'}
+                <p>
+                    <a href="{task.url}" data-sveltekit-prefetch>{task.name}</a><small class="blue-on-green">{task.forkCount} forks</small>
+                </p>
+                <p>
+                    {task.description}
+                </p>
+            {/if}
         {/each}
-      </ul>
+        
+        <h3 class="subtasks">Deeltaken</h3>
+        <ul>
+            {#each tasks as task}
+                {#if task.topic == 'subtask'}
+                    <li>
+                        <a href="{task.url}" data-sveltekit-prefetch>{task.name}</a><small class="blue-on-green">{task.forkCount} forks</small>
+                    </li>
+                {/if}
+            {/each}
+        </ul>
       
+    </section>
+
+    <section class="purple-on-blue showcase">
+        <Heading title="Studentenwerk" />
+
+        {#each tasks as task}
+            {#if task.forks.length}
+                <ul>
+                    {#each task.forks as fork}
+                    
+                    <li>
+                        <strong>
+                            {fork.title}
+                            <IconStar stargazerCount={fork.stargazerCount} />
+                        </strong>
+                        <img src="" alt="{fork.title}">
+                        <a href="{fork.ownerUrl}" target="_blank" rel="noreferrer">@{fork.owner}</a>
+                        <a href="{fork.url}" target="_blank" rel="noreferrer">Code</a>
+                        <a href="{fork.pagesUrl}" target="_blank" rel="noreferrer">Website</a>  
+                    </li>
+                    {/each}
+                </ul>
+            {/if}
+        {/each}
+
     </section>
 {/if}
 
@@ -51,7 +76,7 @@
         border: 2px #66e5bf solid;
         border-radius:1rem
     }
-    :global(section.tasks h3) {
+    :global(section.tasks h3.subtasks) {
         display:none;
 
     }
@@ -78,12 +103,46 @@
         text-overflow: ellipsis;  
         overflow:hidden
     }
+    small {
+        font-size: .5em;
+    }
     section.tasks small {
-      font-size: .5em;
       margin-left: .25rem;
       padding:0 .1rem;
       transform:translateY(-5px);
       white-space: nowrap;
+    }
+    section.showcase {
+        border-radius:1rem;
+        border:2px solid #a675f5
+    }
+    section.showcase img {
+        width:100%;
+        aspect-ratio:2 / 1;
+        display:block;
+        font-size: 0.5rem;
+        background-color: rgba(255,255,255,.2);
+        color:transparent
+
+    }
+    section.showcase li {
+        display:flex;
+        flex-wrap:wrap;
+        margin-top: 1.5rem;
+    }
+    section.showcase li * {
+        white-space: nowrap;
+        margin-right: .25rem;    
+    }
+    section.showcase li strong {
+        width:100%;
+        display:flex;
+    }
+    section.showcase li a {
+       font-size: 0.75em;
+    }
+    section.showcase li strong::first-letter {
+        text-transform:capitalize;
     }
     
 </style>
--- src/routes/[semester]/[slug]/+page.server.js
+++ src/routes/[semester]/[slug]/+page.server.js
@@ -4,37 +4,64 @@ import {headersGitHub, getQueryTasks}  from '$lib/queries/tasks'
 
 let prefix
 
-
 export const load = async ({params: {slug}}) => {
-  prefix = slug
-	const querySprint = getQuerySprint(slug)
-  const queryTasks  = getQueryTasks(slug)
+    prefix = slug
+    const querySprint = getQuerySprint(slug)
+    const queryTasks  = getQueryTasks(slug)
 
-	const dataSprint = await client({ query: querySprint, variables: { slug: slug }, fetch: fetch, endpoint: import.meta.env.VITE_HYPGRAPH_ENDPOINT })
-  const dataTasks  = await client({ query: queryTasks, variables: { slug: slug }, fetch: fetch, endpoint: import.meta.env.VITE_GITHUB_ENDPOINT, headers: headersGitHub })
+    const dataSprint = await client({ query: querySprint, variables: { slug: slug }, fetch: fetch, endpoint: import.meta.env.VITE_HYPGRAPH_ENDPOINT })
+    const dataTasks  = await client({ query: queryTasks, variables: { slug: slug }, fetch: fetch, endpoint: import.meta.env.VITE_GITHUB_ENDPOINT, headers: headersGitHub })
 
-  const tasks = formatTasks(dataTasks, slug)
+    const tasks = formatTasks(dataTasks, slug)
 
-  return { ...dataSprint.sprint, tasks:tasks }
+    return { ...dataSprint.sprint, tasks:tasks }
 }
 
 function formatTasks({search: {repos}}){
-  return repos.map(({repo}) => {
-    const topics = repo.repositoryTopics.edges
-                      .map(topic => topic.node.topic.name)
-                      .filter(topic => topic == 'task' || topic == 'subtask')
-    
-    return {
-      name: formatName(repo.name),
-      description:repo.description,
-      url:repo.url,
-      forkCount:repo.forkCount,
-      topic:topics[0]
-    }
-  })
+    return repos.map(({repo}) => {
+      const topics = repo.repositoryTopics.edges
+                        .map(topic => topic.node.topic.name)
+                        .filter(topic => topic == 'task' || topic == 'subtask')
+      
+      return {
+        name: formatName(repo.name),
+        description:repo.description,
+        url:repo.url,
+        forkCount:repo.forkCount,
+        forks:formatForks(repo),
+        topic:topics[0]
+      }
+    })
 }
 
 function formatName (name) {
-  return name
-            .split(`${prefix}-`).pop().replace(/-/g, " ")
+    if(prefix) {
+        return name.split(`${prefix}-`).pop().replace(/-/g, " ")
+    }
+    else {
+        return name.replace(/-/g, " ")
+    }
+}
+
+function formatForks({forks}) {
+    const ghBaseUrl = 'https://github.com'
+    const ghPagesBaseURL = 'github.io'
+
+    const starredForks =  forks.nodes.filter(node => {
+        return node.stargazerCount > 1
+    })
+    .map(fork => {
+        return {
+            title: formatName(fork.name),
+            name: fork.name,
+            stargazerCount:fork.stargazerCount,
+            owner:fork.owner.login,
+            ownerUrl:fork.owner.url,
+            url:`${ghBaseUrl}/${fork.owner.login}/${fork.name}`,
+            pagesUrl:`https://${fork.owner.login}.${ghPagesBaseURL}/${fork.name}`
+        }        
+    })
+
+    
+    return starredForks
 }
