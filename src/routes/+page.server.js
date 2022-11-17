650b7c7a0e11b7997e97813ca7673dfdf383923f
koopReynders <mail@kopo.nl>
2022-11-17 13:24:42 +0100

    Merge branch 'main' of https://github.com/fdnd/programma.fdnd.nl into main
    Co-authored-by: Joost <joostf@users.noreply.github.com>

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,8 +5,9 @@ import getQueryProgram from '$lib/queries/program'
 export const load = async () => {
     loadingState.set(true)
 	const queryProgram = getQueryProgram()
-    const data = await client({ query: queryProgram, fetch: fetch, endpoint: import.meta.env.VITE_HYPGRAPH_ENDPOINT });
 
+    const data = await client({ query: queryProgram, fetch: fetch, endpoint: import.meta.env.VITE_HYPGRAPH_ENDPOINT });
+    
     return {
         ...data.program
     }
