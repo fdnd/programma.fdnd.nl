a038a7feceab27ddbf7616a71756f6931dd31099
joostf <j.faber@hva.nl>
2023-07-13 11:49:01 +0200

    added 2022 - 2023 in title and menu

---
+++ _app/immutable/chunks/1-72e34815.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-57a6f6bc.js";export{t as component};
--- _app/immutable/chunks/1-72e75745.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-5b36f3a1.js";export{t as component};
