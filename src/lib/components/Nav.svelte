612280c312542888e2d984fddb19bfdfc4cbee98
Koop Reynders <k.g.reynders@hva.nl>
2024-07-15 14:29:14 +0200

    Update Nav voor sprint 1 en 13 van 2425

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,15 +3,15 @@
 	$: navItems = [
 		{
 			href: '/',
-			title: 'Programma 2324'
+			title: 'Programma 2425'
 		},
 		{
-			href: '/data-driven-web/proof-of-concept',
-			title: 'Sprint 12'
+			href: '/static-web/your-tribe',
+			title: 'Sprint 1'
 		},
 		{
-			href: '/meesterproef/final-sprint',
-			title: 'Sprint 20'
+			href: '/workflow-tooling-frameworks/your-tribe-for-life',
+			title: 'Sprint 13'
 		},
 	]
 
