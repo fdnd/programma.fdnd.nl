19b56978908498a4eb238f7aac28e487d7326909
Cyd Stumpel <info@cydstumpel.nl>
2025-04-29 19:31:25 +0200

    gekkigheid

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-auto'
 
 const config = {
 	kit: {
 		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
