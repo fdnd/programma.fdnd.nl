14d45859d19223e2806ef82fea93e78b5001fedd
joostf <j.faber@hva.nl>
2025-11-25 12:31:59 +0100

    Removed console

--- src/routes/[semester]/[sprint]/+page.server.js
+++ src/routes/[semester]/[sprint]/+page.server.js
@@ -14,9 +14,6 @@ export const load = async ({params: {sprint}}) => {
     const dataSprint = await client({ query: querySprint, variables: { slug: sprint }, fetch: fetch, endpoint: import.meta.env.VITE_HYPGRAPH_ENDPOINT })
     const dataTasks  = await client({ query: queryTasks, variables: { slug: sprint }, fetch: fetch, endpoint: import.meta.env.VITE_GITHUB_ENDPOINT, headers: headersGitHub })
     
-    console.log("SPRINT", dataSprint)
-    
-
     const sprintNumber = dataSprint.sprint.sprintNumber
 
     const { prevSprint, nextSprint } = await getSprintNavigation(sprintNumber)
