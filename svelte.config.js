a71f3c79e99407d2b4301d713090f92ab71ce96b
joostf <j.faber@hva.nl>
2025-11-25 09:12:59 +0100

    Migrated to SvelteKit 2.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Config} */
 
 import adapter from '@sveltejs/adapter-netlify'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 const config = {
 	kit: {
