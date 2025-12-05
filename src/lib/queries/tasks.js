5a1ee94ed9c7d47060137e7c34d812cd635363c3
joostf <j.faber@hva.nl>
2025-12-05 09:13:07 +0100

    Cleaning up

--- src/lib/queries/tasks.js
+++ src/lib/queries/tasks.js
@@ -1,26 +1,26 @@
 export const headersGitHub = {
     authorization: 'token ' + import.meta.env.VITE_GITHUB_PERSONAL_ACCESS_TOKEN
 }
+
 export function getQueryTasks(slug) {
-    return `{
-      search(first: 100, type: REPOSITORY, query: "topic:${slug} org:fdnd-task") {
-        pageInfo {
-          hasNextPage
-          endCursor
-        }
-        repos: edges {
-          repo: node {
-            ... on Repository {
-              name
-              description
-              url
-              forkCount
-              repositoryTopics(first: 100) {
-                edges {
-                  node {
-                    topic {
-                      name
-                    }
+  return `{
+    search(first: 100, type: REPOSITORY, query: "topic:${slug} org:fdnd-task") {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      repos: edges {
+        repo: node {
+          ... on Repository {
+            name
+            description
+            url
+            forkCount
+            repositoryTopics(first: 100) {
+              edges {
+                node {
+                  topic {
+                    name
                   }
                 }
               }
@@ -28,5 +28,6 @@ export function getQueryTasks(slug) {
           }
         }
       }
-    }`
+    }
+  }`
 }
